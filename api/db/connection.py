d9dfe4fccd5a278b6674da1a6bb0839e0c640496
john11000 <86936709+john11000@users.noreply.github.com>
2024-11-07 21:29:27 +0000

    Update connection.py

--- api/db/connection.py
+++ api/db/connection.py
@@ -1,7 +1,7 @@
 import pymongo
 
 class Database():
-    client = pymongo.MongoClient('mongodb+srv://jhon:jhon@cluster0.hykao.mongodb.net/?retryWrites=true&w=majority')
+    client = pymongo.MongoClient('mongodb+srv://jhon:jhon@cluster0.pimtz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     def getConnection(self):
         return self.client
 
